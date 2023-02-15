a30edbf4c816df2fe055f1584d73ca872d178ee0
Alexander Lazarenko <ferrosful@gmail.com>
2023-02-15 21:42:53 +0200

    feat: added react-toastify package integration

--- hocs/WithRedux.tsx
+++ hocs/WithRedux.tsx
@@ -1,7 +1,9 @@
 import { persistor, store } from '@store/index';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider as StoreProvider } from 'react-redux';
+
 import PulseLoader from 'react-spinners/PulseLoader';
+import type { OrArray } from 'types/general';
 
 interface WithReduxProps {
   children: OrArray<JSX.Element>;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import '@styles/globals.scss';
 import type { AppProps } from 'next/app';
+
 import WithRedux from '@hocs/WithRedux';
+import { ToastContainer } from 'react-toastify';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <WithRedux>
       <Component {...pageProps} />
+      <ToastContainer position="bottom-right" limit={1} />
     </WithRedux>
   );
 }
