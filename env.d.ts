7556faa0df5db45130c819e7cc9e13e63332a62d
Alexander Lazarenko <ferrosful@gmail.com>
2023-02-15 21:41:13 +0200

    chore: added utility and env variables typedefs

--- env.d.ts
+++ env.d.ts
@@ -1,12 +1,20 @@
 declare global {
   namespace NodeJS {
-    interface ProcessEnv {
+    interface ProcessEnv extends IpRegistryEnv, MailChimpEnv {
       MONGODB_URL: string;
       NODE_ENV: 'development' | 'production';
       BASE_URL: string;
-      IP_REGISTRY_API_KEY: string;
     }
   }
+}
+
+interface IpRegistryEnv {
+  IP_REGISTRY_API_KEY: string;
+}
+
+interface MailChimpEnv {
+  MAILCHIMP_AUDIENCE_ID: string;
+  MAILCHIMP_API_KEY: string;
 }
 
 export {};
--- types/general.d.ts
+++ types/general.d.ts
@@ -1,2 +1,9 @@
-type OrDefaultArray<T> = T | Array<T, undefined>;
+import { NextApiRequest } from 'next';
+
 type OrArray<T> = T | T[];
+type OrDefaultArray<T> = T | Array<T, undefined>;
+
+type OverrideWith<T1, T2> = Omit<T1, keyof T2> & T2;
+type OverrideNextReq<T> = OverrideWith<NextApiRequest, { body: T }>;
+
+export type MyCustomRequest = OverrideWith<NextApiRequest, { body: MyCustomRequestBody }>;
