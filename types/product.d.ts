876c35fc12c0cce95632b0477be46df40df0171d
Alexander Lazarenko <ferrosful@gmail.com>
2023-02-16 14:48:07 +0200

    feat: added temporary mock data for the home page

--- types/product.d.ts
+++ types/product.d.ts
@@ -1,5 +1,14 @@
-export default interface Product {
+interface Product {
   title: string;
   description?: string;
   price: number;
+}
+
+interface FlashDealProduct {
+  id: number;
+  image: string;
+  price: number;
+  discount: number;
+  link: string;
+  amountSold: number;
 }
