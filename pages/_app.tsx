3e0a5db4546d7e149a82fafcb4669e7309b7dbf9
Alexander Lazarenko <ferrosful@gmail.com>
2023-02-18 13:02:14 +0200

    feat: added loading indicator on page transitioning

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,16 @@
 import '@styles/globals.scss';
 import type { AppProps } from 'next/app';
-
 import WithRedux from '@hocs/WithRedux';
+
+import NextNProgress from 'nextjs-progressbar';
 import { ToastContainer } from 'react-toastify';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <WithRedux>
+      <NextNProgress height={6} />
       <Component {...pageProps} />
-      <ToastContainer position="bottom-right" limit={1} />
+      <ToastContainer position="bottom-right" limit={2} />
     </WithRedux>
   );
 }
