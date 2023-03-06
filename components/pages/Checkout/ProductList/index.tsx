679466ae5354ce65bcd5219f189e02336bbbe4d9
Alexander Lazarenko <ferrosful@gmail.com>
2023-03-06 21:17:42 +0200

    style: replaced flex-wrap container with the horizontal scroll section

--- components/pages/Checkout/ProductList/index.tsx
+++ components/pages/Checkout/ProductList/index.tsx
@@ -8,10 +8,10 @@ export default function ProductList({ cart }: ProductListProps) {
   const { products, totalPrice, subTotal } = cart;
 
   return (
-    <div className="products border-greyish border p-4 mt-4">
+    <div className="border-greyish border p-4 mt-4">
       <div className="font-bold text-lg relative flex-between">
         <h1>Your Cart</h1>
-        <p className="absolute right-0 -top-2 flex flex-col">
+        <p className="absolute right-0 -top-3.5 flex flex-col">
           <span>
             {products.length} {products.length === 1 ? 'product' : 'products'}
           </span>
@@ -20,13 +20,13 @@ export default function ProductList({ cart }: ProductListProps) {
           </span>
         </p>
       </div>
-      <div className="wrap flex justify-center md:justify-start flex-wrap gap-4 pt-4">
+      <div className="max-w-full lg:max-w-[680px] xl:max-w-[885px] overflow-x-auto flex gap-4 pt-4">
         {products.map((product) => (
           <ProductListEntry key={product.name} product={product} />
         ))}
       </div>
       <div className="total mt-4 p-2.5 border-t-greyish border-t">
-        Total Price : <b>{totalPrice}$</b>
+        Total Price : <b>{totalPrice.toFixed(2)}$</b>
       </div>
     </div>
   );
