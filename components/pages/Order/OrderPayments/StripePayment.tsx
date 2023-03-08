0cbeec7ba771951f2f352d069514cb8360e45666
Alexander Lazarenko <ferrosful@gmail.com>
2023-03-08 20:51:59 +0200

    feat: implemented stripe payment functionality on credit card payment method choice

--- components/pages/Order/OrderPayments/StripePayment.tsx
+++ components/pages/Order/OrderPayments/StripePayment.tsx
@@ -1,7 +1,58 @@
-export default function StripePayment() {
+import Image from 'next/image';
+import { toast } from 'react-toastify';
+import stripePromise from '@lib/stripe';
+
+import { usePayWithStripeMutation } from '@store/api';
+import PropagateLoader from 'react-spinners/PropagateLoader';
+
+interface StripePaymentProps {
+  order: OrderModel;
+}
+
+export default function StripePayment({ order }: StripePaymentProps) {
+  const [putPayment, { isLoading, error }] = usePayWithStripeMutation();
+
+  const createCheckoutSession = async () => {
+    const stripe = await stripePromise;
+
+    const { data } = (await putPayment({
+      orderId: order._id,
+      email: order.user.email,
+      products: order.products,
+    })) as { data: StripeOrderResponse };
+
+    const result = await stripe?.redirectToCheckout({
+      sessionId: data?.id,
+    });
+
+    if (result?.error) {
+      toast.error(result?.error.message ?? 'Payment error occurred!');
+    }
+  };
+
   return (
-    <div>
-      <span />
+    <div className="min-h-[150px] flex-between flex-col gap-2">
+      <Image
+        src="/images/payments/stripe.png"
+        className="rounded-md border-greyish border h-20"
+        alt="Stripe Payment"
+        width={400}
+        height={100}
+        loading="lazy"
+      />
+      <button
+        type="submit"
+        onClick={createCheckoutSession}
+        className={`w-full h-12 p-3 transition-colors duration-300 bg-blue hover:bg-blue-dark text-white uppercase rounded-md ${
+          error ? 'bg-error-secondary pointer-events-none' : ''
+        } ${isLoading ? 'bg-grey-lighter pointer-events-none' : ''}`}
+      >
+        {isLoading ? (
+          <PropagateLoader color="white" size={10} className="pb-[13px]" />
+        ) : (
+          'pay with credit card'
+        )}
+      </button>
     </div>
   );
 }
--- components/pages/Order/OrderPayments/index.tsx
+++ components/pages/Order/OrderPayments/index.tsx
@@ -5,7 +5,7 @@ import PayPalPayment from './PayPalPayment';
 import StripePayment from './StripePayment';
 
 interface OrderPaymentsProps {
-  order: Omit<OrderModel, 'products'>;
+  order: OrderModel;
 }
 
 export default function OrderPayments({ order }: OrderPaymentsProps) {
@@ -16,10 +16,10 @@ export default function OrderPayments({ order }: OrderPaymentsProps) {
       options={{ 'client-id': process.env.PAYPAL_CLIENT_ID }}
       deferLoading={false}
     >
-      {wasPaid && (
+      {!wasPaid && (
         <div className="p-4 shadow-md">
           {paymentMethod === 'paypal' && <PayPalPayment order={order} />}
-          {paymentMethod === 'credit_card' && <StripePayment />}
+          {paymentMethod === 'credit_card' && <StripePayment order={order} />}
           {paymentMethod === 'cash' && (
             <div className="mt-2 flex flex-col items-center">
               <AiOutlineDollar className="w-8 h-8 -mb-3 fill-green" />
