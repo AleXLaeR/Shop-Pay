4bf4d33100d984de41ff87c2a3a5f60234def9da
Alexander Lazarenko <ferrosful@gmail.com>
2023-03-11 11:30:43 +0200

    feat: added auth-protecting middleware

---
+++ middleware.ts
@@ -0,0 +1,9 @@
+import { withAuth } from 'next-auth/middleware';
+
+export default withAuth({
+  callbacks: {
+    authorized: ({ token }) => !!token,
+  },
+});
+
+export const config = { matcher: ['/checkout', '/order/(.*)', '/profile'] };
